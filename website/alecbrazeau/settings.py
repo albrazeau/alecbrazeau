73170e701f6861894c47017a047a42121a813535
albrazeau <abrazeau1993@gmail.com>
2020-04-25 15:50:20 -0400

    animate landing page title

--- website/alecbrazeau/settings.py
+++ website/alecbrazeau/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from . import mysecrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-*we5p=@1v(_9fkam=kg40fp9fd2!9f4^52u@()$x5s)3-tfld'
+SECRET_KEY = mysecrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- website/static/js/landingpage.js
+++ website/static/js/landingpage.js
@@ -1,3 +1,34 @@
-function anmiateTitle(id) {
-    id.innerHTML = "ALEC BRAZEAU"
+function titleLoad() {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.landing-title-text');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline()
+    .add({
+        targets: '.landing-title-text .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 1500,
+        delay: (el, i) => 150 * (i+1)
+    });
+}
+
+function titleClick(){
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.landing-title-text');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline()
+    .add({
+        targets: '.landing-title-text .letter',
+        opacity: [1,0],
+        easing: "easeOutExpo",
+        duration: 800,
+        delay: (el, i) => 150 * (i+1)
+    }).add({
+        targets: '.landing-title-text .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 800,
+        delay: (el, i) => 150 * (i+1)
+    });
 }
